9a48b2510ef0b9f5f40fc1c3d0b8deabf8f225d6
Koua Ekaza Wilfried <wilfried.koua@futurafric.com>
2021-08-03 13:55:29 +0200

    such

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/dashboard/index/index.component';
+import { HomeComponent } from './pages/guest/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+
+  {
+    path:'dashboard',
+    component:IndexComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/guest/home/home.component';
+import { AboutComponent } from './pages/guest/about/about.component';
+import { IndexComponent } from './pages/dashboard/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
